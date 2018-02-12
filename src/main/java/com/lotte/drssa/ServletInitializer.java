03bd2fb0180ca33e1eadbd7fd423dc6a229c6397
DESKTOP-09AP62D\user <mujuk73@gmail.com>
2018-02-12 21:58:44 +0900

    no message

---
+++ src/main/java/com/lotte/drssa/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lotte.drssa;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+}
